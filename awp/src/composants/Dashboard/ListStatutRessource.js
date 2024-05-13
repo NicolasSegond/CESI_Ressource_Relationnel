a2d06fdede4ff07c7807db7b6c757d1fa94da718
mathiscolbaut <mathiscolbaut@gmail.com>
2024-05-13 20:50:27 +0200

    KAN-49 ListeRessourceTrieStatutRessource
    *ajout Des ressources et trié par statut vos ressources

---
+++ awp/src/composants/Dashboard/ListStatutRessource.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from 'react';
+import { CircularProgress, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import DataTable from '../Administration/Gestion/Ressource/dataTable';
+import PaginationGlobal from '../General/PaginationGlobal';
+import apiConfig from "../../utils/config";
+import { Link } from "react-router-dom";
+import { customFetch } from "../../utils/customFetch";
+import { getIdUser, getToken } from "../../utils/authentification";
+
+const ListStatutRessource = ({ title, ownerId, statusId }) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [tableWidth, setTableWidth] = useState(0);
+    const [selectedStatus, setSelectedStatus] = useState(statusId); // État pour stocker l'ID du statut sélectionné
+    const [statusList, setStatusList] = useState([]); // État pour stocker la liste des statuts
+
+    useEffect(() => {
+        // Fonction pour récupérer la liste des statuts depuis l'API
+        const fetchStatusList = async () => {
+            try {
+                const { data: statusData } = await customFetch({
+                    url: `${apiConfig.apiUrl}/api/statuts`,
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }, true);
+                setStatusList(statusData['hydra:member']);
+            } catch (error) {
+                console.error('Error fetching status list:', error);
+            }
+        };
+
+        fetchStatusList();
+    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+
+            try {
+                const { data: responseData, error } = await customFetch({
+                    url: `${apiConfig.apiUrl}/api/ressources?page=${currentPage}&proprietaire=${ownerId}&statut=${selectedStatus}`,
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/ld+json',
+                    },
+                }, true);
+
+                if (error) {
+                    console.error('Error fetching data:', error);
+                    setData([]);
+                } else {
+                    setData(responseData['hydra:member']);
+
+                    if (responseData['hydra:view']) {
+                        const lastPageUrl = responseData['hydra:view']['hydra:last'];
+                        const totalPages = lastPageUrl ? parseInt(lastPageUrl.split('=').pop()) : 1;
+                        setTotalPages(totalPages);
+                    } else {
+                        setTotalPages(1);
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setData([]);
+            }
+
+            setLoading(false);
+        };
+
+        fetchData();
+    }, [currentPage, ownerId, selectedStatus]);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const handleStatusChange = (event) => {
+        setSelectedStatus(event.target.value);
+    };
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
+    const renderLink = (row) => {
+        return (
+            <Link to={`/ressource/${row.id}`}>{row.titre}</Link>
+        );
+    };
+
+    const renderStatusSelect = () => (
+        <FormControl fullWidth style={{ maxWidth: '100px', marginleft: '200px' }}>
+            <InputLabel id="status-select-label">Statut</InputLabel>
+            <Select
+                labelId="status-select-label"
+                id="status-select"
+                value={selectedStatus}
+                onChange={handleStatusChange}
+            >
+                {statusList.map(status => (
+                    <MenuItem key={status.id} value={status.id}>{status.nomStatut}</MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+
+
+
+    const columns = [
+        { label: 'Titre', render: renderLink },
+        { render: row => formatDate(row.dateCreation), label: 'Date de Création' },
+        { render: row => formatDate(row.dateModification), label: 'Date de Modification' },
+        { label: 'Nombre de vue', render: row => row.nombreVue },
+        { label: 'Statut', render: row => row.statut.nomStatut },
+    ];
+
+    return (
+        <div>
+            <h2 style={{ textAlign: 'center' }}>{title}</h2>
+
+            <div style={{ marginBottom: '20px', marginTop: '20px' }}>
+                {renderStatusSelect()}
+            </div>
+
+            <div style={{ position: 'relative' }}>
+                {loading && (
+                    <CircularProgress
+                        style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            zIndex: 1,
+                        }}
+                    />
+                )}
+
+                <DataTable
+                    data={data}
+                    loading={loading}
+                    columns={columns}
+                    onTableWidthChange={setTableWidth}
+                />
+            </div>
+
+            {totalPages > 1 && (
+                <PaginationGlobal
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={handlePageChange}
+                />
+            )}
+        </div>
+    );
+};
+
+export default ListStatutRessource;
--- awp/src/page/DashBoardUser/Dashboard.js
+++ awp/src/page/DashBoardUser/Dashboard.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import ListTypeProgression from "../../composants/Dashboard/ListTypeProgression";
+import ListStatutRessource from "../../composants/Dashboard/ListStatutRessource";
+import {getIdUser, getToken, getTokenDisconnected} from "../../utils/authentification";
 
 function Dashboard() {
-    const titleLeft = "Favorie";
-    const idLeft = 1; // Exemple d'ID
-    const titleRight = "Mise de côté";
-    const idRight = 2; // Exemple d'ID
-
+    const titleLeft = "Favoris";
+    const idLeft = 1;
+    const titleRight = "Mises de côté";
+    const idRight = 2;
+    const idStatut = 2;
+    const titleStatut = "Vos ressources";
+    const UserConnect = getIdUser(getTokenDisconnected());
+    console.log(UserConnect);
     const containerStyle = {
         textAlign: 'center',
+        backgroundColor: '#f2f2f2', // Couleur de fond de la page
+        padding: '20px', // Espacement intérieur
     };
 
     const listContainerStyle = {
         display: 'inline-block',
-        marginRight: '20px', // Espace entre les deux composants
-        verticalAlign: 'top', // Aligner les composants en haut
+        marginRight: '20px',
+        verticalAlign: 'top',
     };
 
     return (
         <div style={containerStyle}>
-            <h2>Dashboard</h2>
-            <br />
-            <div style={{marginLeft: '20px'}}>
+            <h1>Dashboard</h1>
+            <br/>
+            <div style={{marginLeft: '20px', marginTop: '20px'}}>
+
+                <div style={listContainerStyle}>
+                    <ListStatutRessource title={titleStatut} ownerId={UserConnect} statusId={idStatut}/>
+                </div>
+            </div>
+            <div style={{marginLeft: '20px', marginTop: '20px'}}>
                 <div>
                     <div style={listContainerStyle}>
-                        <ListTypeProgression title={titleLeft} id={idLeft} />
+                        <ListTypeProgression title={titleLeft} id={idLeft}/>
                     </div>
                     <div style={listContainerStyle}>
-                        <ListTypeProgression title={titleRight} id={idRight} />
+                        <ListTypeProgression title={titleRight} id={idRight}/>
                     </div>
                 </div>
+
             </div>
         </div>
     );
