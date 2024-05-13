8c7649dee64e74b799db6b1bccdd18f3f8afff05
mathiscolbaut <mathiscolbaut@gmail.com>
2024-05-13 21:32:47 +0200

    PREPRODDEV-CorrectionBugAndroidHTML-Manifest

--- mobile/ressources_re_mobile/lib/utilities/authentification.dart
+++ mobile/ressources_re_mobile/lib/utilities/authentification.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:html';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decode/jwt_decode.dart';
