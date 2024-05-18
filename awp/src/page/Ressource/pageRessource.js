bf56dd4a9ffa9bbedba67ab53e287c273b16bf31
Nicolas <nicolassegond0@gmail.com>
2024-05-18 17:48:01 +0200

    KAN-76 : Commentaires + pièces jointes react

--- awp/src/page/Ressource/pageRessource.js
+++ awp/src/page/Ressource/pageRessource.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import {useLocation, useParams} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
 import styles from './pageRessource.module.css'
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import EditIcon from '@mui/icons-material/Edit';
@@ -8,41 +8,81 @@ import PersonIcon from '@mui/icons-material/Person';
 import CategoryIcon from '@mui/icons-material/Category';
 import LabelIcon from '@mui/icons-material/Label';
 import {getIdUser, getTokenDisconnected} from "../../utils/authentification";
+import apiConfig from "../../utils/config";
+import {customFetch} from "../../utils/customFetch";
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 function PageRessource() {
     const location = useLocation();
-    const { id } = location.state || {};
+    const {id} = location.state || {};
     const [ressource, setRessource] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [userId, setUserId] = useState(null);
     const [commentContent, setCommentContent] = useState('');
+    const [commentaire, setCommentaire] = useState([]);
 
+    const fetchCommentaire = async () => {
+        try {
+            // Construit l'URL en fonction des filtres sélectionnés
+            let url = apiConfig.apiUrl + `/api/commentaires/` + id + '/ressources?&order%5Bdate%5D=desc';
 
+            // Effectue la requête POST à l'API
+            const {data, error} = await customFetch({
+                url: url,
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }, false);
+
+            if (error) {
+                console.error('Erreur lors de la modification de la catégorie:', error);
+            } else {
+                setCommentaire(data['hydra:member']);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la modification de la catégorie:', error);
+        }
+    }
 
     useEffect(() => {
         const token = getTokenDisconnected();
+
+        console.log(token);
+
         if (token) {
             setUserId(getIdUser(token));
         }
 
-        fetch(`http://127.0.0.1:8000/api/ressources/${id}`)
-            .then(response => response.json())
-            .then(data => {
-                setRessource(data);
-                setLoading(false);
-            })
-            .catch(err => {
-                console.error("Failed to fetch resource:", err);
-                setError(err);
-                setLoading(false);
-            });
+        customFetch(
+            {
+                url: apiConfig.apiUrl + `/api/ressources/${id}`,
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+            false
+        ).then(({data, error}) => {
+                console.log(data, error);
+                if (error) {
+                    console.error("Failed to fetch resource:", error);
+                    setError(error);
+                    setLoading(false);
+                } else {
+                    setRessource(data);
+                    setLoading(false);
+                    fetchCommentaire();
+                    console.log(commentaire);
+                }
+            }
+        )
     }, [id]);
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error loading the resource.</div>;
     if (!ressource) return <div>No resource found.</div>;
-    const sortedComments = ressource.commentaires.sort((a, b) => new Date(a.date) - new Date(b.date));
 
 
     //Attention ici à refaire !!
@@ -52,28 +92,31 @@ function PageRessource() {
 
         const commentData = {
             contenu: commentContent,
-            date: new Date().toISOString(), // Suppose the server accepts date in this format
-            // Add other necessary fields here
+            utilisateur: `/api/utilisateurs/${userId}`,
+            ressource: `/api/ressources/${ressource.id}`,
+            date: new Date().toISOString(),
         };
-        //Attention faire Car pas le back
-        fetch(`http://127.0.0.1:8000/api/commentaires`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${getTokenDisconnected()}`
-            },
-            body: JSON.stringify(commentData)
-        })
-            .then(response => response.json())
-            .then(newComment => {
-                setRessource(prevRessource => ({
-                    ...prevRessource,
-                    commentaires: [...prevRessource.commentaires, newComment]
-                }));
-                setCommentContent('');
-            })
-            .catch(error => {
-                console.error("Error posting comment:", error);
+
+        customFetch(
+            {
+                url: apiConfig.apiUrl + `/api/commentaires`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(commentData)
+            }, false
+        )
+            .then(({data, error}) => {
+                if (error) {
+                    console.error("Error posting comment:", error);
+                } else {
+                    setRessource(prevRessource => ({
+                        ...prevRessource,
+                        commentaires: [...prevRessource.commentaires, data]
+                    }));
+                    fetchCommentaire();
+                }
             });
     }
 
@@ -84,38 +127,51 @@ function PageRessource() {
             <img className={styles["article-image"]} src={`http://127.0.0.1:8000/images/book/${ressource.miniature}`}
                  alt="Miniature"/>
             <div className={styles["article-metadata"]}>
-                <p><AccessTimeIcon style={{verticalAlign: 'middle'}}/> Created
-                    on: {new Date(ressource.dateCreation).toLocaleDateString()}</p>
-                <p><EditIcon style={{verticalAlign: 'middle'}}/> Last
-                    modified: {new Date(ressource.dateModification).toLocaleDateString()}</p>
-                <p><VisibilityIcon style={{verticalAlign: 'middle'}}/> Views: {ressource.nombreVue}</p>
+                <p><AccessTimeIcon style={{verticalAlign: 'middle'}}/> Crée
+                    le : {new Date(ressource.dateCreation).toLocaleDateString()}</p>
+                <p><EditIcon style={{verticalAlign: 'middle'}}/> Dernière modification
+                    : {new Date(ressource.dateModification).toLocaleDateString()}</p>
+                <p><VisibilityIcon style={{verticalAlign: 'middle'}}/> Vues : {ressource.nombreVue}</p>
                 <p><PersonIcon
-                    style={{verticalAlign: 'middle'}}/> Owner: {ressource.proprietaire.nom} {ressource.proprietaire.prenom}
+                    style={{verticalAlign: 'middle'}}/> Propriétaire
+                    : {ressource.proprietaire.nom} {ressource.proprietaire.prenom}
                 </p>
                 <p><LabelIcon style={{verticalAlign: 'middle'}}/> Type: {ressource.typeDeRessource.libelle}</p>
-                <p><CategoryIcon style={{verticalAlign: 'middle'}}/> Category: {ressource.categorie.nom}</p>
+                <p><CategoryIcon style={{verticalAlign: 'middle'}}/> Categorie : {ressource.categorie.nom}</p>
             </div>
 
             <div className={styles["article-content"]} dangerouslySetInnerHTML={{__html: ressource.contenu}}/>
+            <div className={styles["attachments-section"]}>
+                <h2>Pièces jointes</h2>
+                {ressource.fichiers && ressource.fichiers.map((fichier, index) => (
+                    <div key={index} className={styles["attachment"]}>
+                        <AttachFileIcon/>
+                        <a href={`${apiConfig.apiUrl}/images/book/${fichier.nom}`} target="_blank"
+                           rel="noopener noreferrer">
+                            {fichier.nom}
+                        </a>
+                    </div>
+                ))}
+            </div>
             <div className={styles["comment-section"]}>
                 <h2>Commentaires</h2>
-                {sortedComments.map((comment, index) => (
+                {commentaire.map((comment, index) => (
                     <div key={index} className={styles["comment"]}>
                         <p>{comment.contenu}</p>
-                        <p className={styles["comment-date"]}>Posted
-                            on: {new Date(comment.date).toLocaleDateString()}</p>
+                        <p className={styles["comment-date"]}>Poster
+                            le : {new Date(comment.date).toLocaleDateString()}</p>
                     </div>
                 ))}
                 <div className={styles["comment-form"]}>
                     {userId && (
                         <form onSubmit={handleCommentSubmit}>
-            <textarea
-                value={commentContent}
-                onChange={e => setCommentContent(e.target.value)}
-                placeholder="Ajoutez un commentaire..."
-                className={styles["comment-input"]}
-                required
-            />
+                            <textarea
+                                value={commentContent}
+                                onChange={e => setCommentContent(e.target.value)}
+                                placeholder="Ajoutez un commentaire..."
+                                className={styles["comment-input"]}
+                                required
+                            />
                             <button type="submit" className={styles["submit-comment"]}>Poster le commentaire</button>
                         </form>
                     )}
