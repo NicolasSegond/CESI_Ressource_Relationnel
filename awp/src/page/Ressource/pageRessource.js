332b627723432dceeef4e56dff9091c06b4b59bd
mathiscolbaut <mathiscolbaut@gmail.com>
2024-05-20 14:28:24 +0200

    PROPROD Securisation Ajout Commentaire React
    *Empêcher le spam de commentaire avec un couldown
    *Limitations comme sur flutter à un minimum de 30 caractères

--- awp/src/page/Ressource/pageRessource.js
+++ awp/src/page/Ressource/pageRessource.js
@@ -1,16 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import {useLocation, useParams} from 'react-router-dom';
-import styles from './pageRessource.module.css'
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import styles from './pageRessource.module.css';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import PersonIcon from '@mui/icons-material/Person';
 import CategoryIcon from '@mui/icons-material/Category';
 import LabelIcon from '@mui/icons-material/Label';
-import {getIdUser, getTokenDisconnected} from "../../utils/authentification";
-import apiConfig from "../../utils/config";
-import {customFetch} from "../../utils/customFetch";
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { getIdUser, getTokenDisconnected } from "../../utils/authentification";
+import apiConfig from "../../utils/config";
+import { customFetch } from "../../utils/customFetch";
+import CustomAlert from "../../composants/CustomAlert";
 
 function PageRessource() {
     const { id } = useParams();
@@ -20,14 +21,20 @@ function PageRessource() {
     const [userId, setUserId] = useState(null);
     const [commentContent, setCommentContent] = useState('');
     const [commentaire, setCommentaire] = useState([]);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [alerts, setAlerts] = useState([]);
+
+    const addAlert = (severity, message) => {
+        setAlerts(prevAlerts => [...prevAlerts, { severity, message }]);
+    };
+    const handleCloseAlert = (index) => {
+        setAlerts(prevAlerts => prevAlerts.filter((_, i) => i !== index));
+    };
 
     const fetchCommentaire = async () => {
         try {
-            // Construit l'URL en fonction des filtres sélectionnés
-            let url = apiConfig.apiUrl + `/api/commentaires/` + id + '/ressources?&order%5Bdate%5D=desc';
-
-            // Effectue la requête POST à l'API
-            const {data, error} = await customFetch({
+            let url = `${apiConfig.apiUrl}/api/commentaires/${id}/ressources?&order%5Bdate%5D=desc`;
+            const { data, error } = await customFetch({
                 url: url,
                 method: 'GET',
                 headers: {
@@ -36,19 +43,17 @@ function PageRessource() {
             }, false);
 
             if (error) {
-                console.error('Erreur lors de la modification de la catégorie:', error);
+                console.error('Erreur lors de la récupération des commentaires:', error);
             } else {
                 setCommentaire(data['hydra:member']);
             }
         } catch (error) {
-            console.error('Erreur lors de la modification de la catégorie:', error);
+            console.error('Erreur lors de la récupération des commentaires:', error);
         }
-    }
+    };
 
     useEffect(() => {
         const token = getTokenDisconnected();
-
-        console.log(token);
 
         if (token) {
             setUserId(getIdUser(token));
@@ -56,38 +61,45 @@ function PageRessource() {
 
         customFetch(
             {
-                url: apiConfig.apiUrl + `/api/ressources/${id}`,
+                url: `${apiConfig.apiUrl}/api/ressources/${id}`,
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                 },
             },
             false
-        ).then(({data, error}) => {
-                console.log(data, error);
-                if (error) {
-                    console.error("Failed to fetch resource:", error);
-                    setError(error);
-                    setLoading(false);
-                } else {
-                    setRessource(data);
-                    setLoading(false);
-                    fetchCommentaire();
-                    console.log(commentaire);
-                }
+        ).then(({ data, error }) => {
+            if (error) {
+                console.error("Failed to fetch resource:", error);
+                setError(error);
+                setLoading(false);
+            } else {
+                setRessource(data);
+                setLoading(false);
+                fetchCommentaire();
             }
-        )
+        });
     }, [id]);
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error loading the resource.</div>;
     if (!ressource) return <div>No resource found.</div>;
 
-
-    //Attention ici à refaire !!
-    function handleCommentSubmit(e) {
+    const handleCommentSubmit = (e) => {
         e.preventDefault();
-        if (!commentContent.trim()) return;
+
+        if (isSubmitting) {
+            addAlert('info', 'Veuillez attendre 10 secondes avant de soumettre à nouveau.');
+            return;
+        }
+
+        if (commentContent.length < 30) {
+            addAlert('info', 'Le commentaire doit contenir au moins 30 caractères.');
+            return;
+        }
+
+        setIsSubmitting(true);
+        setTimeout(() => setIsSubmitting(false), 10000);
 
         const commentData = {
             contenu: commentContent,
@@ -98,7 +110,7 @@ function PageRessource() {
 
         customFetch(
             {
-                url: apiConfig.apiUrl + `/api/commentaires`,
+                url: `${apiConfig.apiUrl}/api/commentaires`,
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -106,47 +118,36 @@ function PageRessource() {
                 body: JSON.stringify(commentData)
             }, false
         )
-            .then(({data, error}) => {
+            .then(({ data, error }) => {
                 if (error) {
                     console.error("Error posting comment:", error);
                 } else {
-                    setRessource(prevRessource => ({
-                        ...prevRessource,
-                        commentaires: [...prevRessource.commentaires, data]
-                    }));
-                    fetchCommentaire();
+                    setCommentaire(prevCommentaire => [...prevCommentaire, data]);
+                    setCommentContent('');
                 }
             });
-    }
-
+    };
 
     return (
         <div className={styles["page-container"]}>
             <h1 className={styles["header"]}>{ressource.titre}</h1>
-            <img className={styles["article-image"]} src={`http://127.0.0.1:8000/images/book/${ressource.miniature}`}
-                 alt="Miniature"/>
+            <img className={styles["article-image"]} src={`http://127.0.0.1:8000/images/book/${ressource.miniature}`} alt="Miniature" />
             <div className={styles["article-metadata"]}>
-                <p><AccessTimeIcon style={{verticalAlign: 'middle'}}/> Crée
-                    le : {new Date(ressource.dateCreation).toLocaleDateString()}</p>
-                <p><EditIcon style={{verticalAlign: 'middle'}}/> Dernière modification
-                    : {new Date(ressource.dateModification).toLocaleDateString()}</p>
-                <p><VisibilityIcon style={{verticalAlign: 'middle'}}/> Vues : {ressource.nombreVue}</p>
-                <p><PersonIcon
-                    style={{verticalAlign: 'middle'}}/> Propriétaire
-                    : {ressource.proprietaire.nom} {ressource.proprietaire.prenom}
-                </p>
-                <p><LabelIcon style={{verticalAlign: 'middle'}}/> Type: {ressource.typeDeRessource.libelle}</p>
-                <p><CategoryIcon style={{verticalAlign: 'middle'}}/> Categorie : {ressource.categorie.nom}</p>
+                <p><AccessTimeIcon style={{ verticalAlign: 'middle' }} /> Crée le : {new Date(ressource.dateCreation).toLocaleDateString()}</p>
+                <p><EditIcon style={{ verticalAlign: 'middle' }} /> Dernière modification : {new Date(ressource.dateModification).toLocaleDateString()}</p>
+                <p><VisibilityIcon style={{ verticalAlign: 'middle' }} /> Vues : {ressource.nombreVue}</p>
+                <p><PersonIcon style={{ verticalAlign: 'middle' }} /> Propriétaire : {ressource.proprietaire.nom} {ressource.proprietaire.prenom}</p>
+                <p><LabelIcon style={{ verticalAlign: 'middle' }} /> Type: {ressource.typeDeRessource.libelle}</p>
+                <p><CategoryIcon style={{ verticalAlign: 'middle' }} /> Categorie : {ressource.categorie.nom}</p>
             </div>
 
-            <div className={styles["article-content"]} dangerouslySetInnerHTML={{__html: ressource.contenu}}/>
+            <div className={styles["article-content"]} dangerouslySetInnerHTML={{ __html: ressource.contenu }} />
             <div className={styles["attachments-section"]}>
                 <h2>Pièces jointes</h2>
                 {ressource.fichiers && ressource.fichiers.map((fichier, index) => (
                     <div key={index} className={styles["attachment"]}>
-                        <AttachFileIcon/>
-                        <a href={`${apiConfig.apiUrl}/images/book/${fichier.nom}`} target="_blank"
-                           rel="noopener noreferrer">
+                        <AttachFileIcon />
+                        <a href={`${apiConfig.apiUrl}/images/book/${fichier.nom}`} target="_blank" rel="noopener noreferrer">
                             {fichier.nom}
                         </a>
                     </div>
@@ -157,8 +158,7 @@ function PageRessource() {
                 {commentaire.map((comment, index) => (
                     <div key={index} className={styles["comment"]}>
                         <p>{comment.contenu}</p>
-                        <p className={styles["comment-date"]}>Poster
-                            le : {new Date(comment.date).toLocaleDateString()}</p>
+                        <p className={styles["comment-date"]}>Posté le : {new Date(comment.date).toLocaleDateString()}</p>
                     </div>
                 ))}
                 <div className={styles["comment-form"]}>
@@ -176,6 +176,15 @@ function PageRessource() {
                     )}
                 </div>
             </div>
+            {alerts.map((alert, index) => (
+                <CustomAlert
+                    key={index}
+                    open={true}
+                    message={alert.message}
+                    handleClose={() => handleCloseAlert(index)}
+                    severity={alert.severity}
+                />
+            ))}
         </div>
     );
 }
